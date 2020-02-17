a697e0d5585a5817e94e72eed1c149bdcfc1f930
Georgios Pitsiladis <g.v.pitsiladis@gmail.com>
2020-02-17 23:06:43 +0200

    Add gitignore, README, and setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="duplicatecolumns",
+    version="0.0.1",
+    description="Duplicate columns with all their values",
+    author="Adam Hooper",
+    author_email="adam@adamhooper.com",
+    url="https://github.com/CJWorkbench/converttexttonumber",
+    packages=[""],
+    py_modules=["duplicatecolumns"],
+    install_requires=["pandas==0.25.0"],
+)
